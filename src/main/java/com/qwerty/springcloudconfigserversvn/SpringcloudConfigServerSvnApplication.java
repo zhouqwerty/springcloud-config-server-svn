c390dbb82fde80d9d0e1953d2ec81ead55eca8f5
zhou <2891128968@qq.com>
2019-06-08 19:21:40 +0800

    2019.6.8 14:32 注册中心注册配置中心服务端

--- src/main/java/com/qwerty/springcloudconfigserversvn/SpringcloudConfigServerSvnApplication.java
+++ src/main/java/com/qwerty/springcloudconfigserversvn/SpringcloudConfigServerSvnApplication.java
@@ -3,7 +3,11 @@ package com.qwerty.springcloudconfigserversvn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+/*
+*  /actuator/refresh:刷新自己当前这个节点
+* /actuator/bus-refresh:刷新所有节点
+* /actuator/bus-refresh/appName:port:刷新某个节点 如果port位置是** 刷新指定appName服务的所有节点
+* */
 @SpringBootApplication
 @EnableConfigServer
 public class SpringcloudConfigServerSvnApplication {
